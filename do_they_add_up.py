24d94409646f6d88b9174ab2204060b8aa238c43
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-01-29 13:21:08 -0600

    do_they_add_up.py

---
+++ do_they_add_up.py
@@ -0,0 +1,17 @@
+import random
+
+
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+def is_num_in_numbers(k, nums):
+    matches = []
+    for i in range(0, len(nums) - 1):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == k:
+                matches.append(f"[{i},{j}]:({nums[i]},{nums[j]})")
+    return matches
+
+
+numbers = [random.randint(1, 8) for x in range(1, 10)]
+print(numbers)
+print(f"Matches: {is_num_in_numbers(int(input()), numbers)}")
