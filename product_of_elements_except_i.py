c61af30fa4b2cb87d5ccdc93ec177991ca9bd267
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-01-31 17:54:45 -0600

    product_of_elements_except_i.py

--- product_of_elements_except_i.py
+++ product_of_elements_except_i.py
@@ -14,6 +14,32 @@ def product_array():
     return [math.floor(product / x) for x in numbers]
 
 
+def product_array_2():
+    prefix_pdts = []
+    suffix_pdts = []
+    for num in numbers:
+        if prefix_pdts:
+            prefix_pdts.append(num * prefix_pdts[-1])
+        else:
+            prefix_pdts.append(num)
+    for num in reversed(numbers):
+        if suffix_pdts:
+            suffix_pdts.append(num * suffix_pdts[-1])
+        else:
+            suffix_pdts.append(num)
+    suffix_pdts = list(reversed(suffix_pdts))
+    result = []
+    for i in range(len(numbers)):
+        if i == 0:
+            result.append(suffix_pdts[i + 1])
+        elif i == len(numbers) - 1:
+            result.append(prefix_pdts[i - 1])
+        else:
+            result.append(prefix_pdts[i - 1] + suffix_pdts[i + 1])
+
+
 numbers = [random.randint(1, 10) for x in range(1, 6)]
 print(numbers)
 print(product_array())
+print(product_array_2())
+
